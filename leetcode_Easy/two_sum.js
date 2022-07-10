c2adb10f4591020ffb3515dc6a3f543e252b7592
Mike <MikeKonvalinka@gmail.com>
2022-07-10 00:34:02 -0500

    updated the answer for two_sum

--- leetcode_Easy/two_sum.js
+++ leetcode_Easy/two_sum.js
@@ -1,8 +1,25 @@
 // https://leetcode.com/problems/two-sum/
 // Given an array of Numbers & a target, return the array locations that equal up to the target. ([1,4,5,15], 9) return [1,2]. Can assume only one solution & each number only used once
 
+// After reviewing solutions to this problem on leetcode, I changed a few things around including the handling of the return value
+// I also removed the break & the return array as they were unnecessary
+twoSum = (nums,target) => {
+    if (nums.length < 2){
+        return("there are not enough numbers in the array to complete this")
+    }
+    for (let x = 0; x < nums.length-1; x++) {
+        for (let y = x+1; y < nums.length; y++){
+            let targetValue = nums[x] + nums[y];
+            if (targetValue === target){
+             return ([x,y])   
+            }                
+            }
+        }
+    return("there is no valid set of nums");
+    };
 
- twoSums = (inputArray,target) => {
+/* 
+twoSum = (inputArray,target) => {
     // Declare returnArray variable to be able to be able to store the two array values
     let returnArray = [];
     // Loop through each possible combos in the array to find which combo adds to the target
@@ -21,10 +38,10 @@
     return(returnArray);
     console.log(returnArray)
 }
+*/
 
 
 
-twoSums([1,2,3,4],7)
-twoSums([0,5,9,1,4,67,3,48,43,500,1,7,97],509)
-
+console.log(twoSums([1,2,3,4],7))
+console.log(twoSums([0,5,9,1,4,67,3,48,43,500,1,7,97],509))
 
